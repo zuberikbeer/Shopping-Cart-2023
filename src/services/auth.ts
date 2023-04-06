13ee8e9474727fb21a3a9545a1e8eb3c21535102
Jordan Khalil <jordankhalil0@gmail.com>
2023-04-06 15:08:15 -0400

    update1

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,18 +1,18 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000"; // Replace this with the URL of your backend server
+const baseUrl: string = process.env.REACT_APP_API_BASE_URL || "";
 
 export const register = async (email: string, password: string) => {
-  const response = await axios.post(`${API_URL}/register`, { email, password });
+  const response = await axios.post(`${baseUrl}/register`, { email, password });
   return response.data;
 };
 
 export const login = async (email: string, password: string) => {
-  const response = await axios.post(`${API_URL}/login`, { email, password });
+  const response = await axios.post(`${baseUrl}/login`, { email, password });
   return response.data;
 };
 
 export const googleSignIn = async (tokenId: string) => {
-  const response = await axios.post(`${API_URL}/googleSignIn`, { tokenId });
+  const response = await axios.post(`${baseUrl}/googleSignIn`, { tokenId });
   return response.data;
 };
--- src/services/shoppingCartService.ts
+++ src/services/shoppingCartService.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const baseUrl: string = process.env.REACT_APP_API_BASE_URL || "";
+
 export const getShoppingCartItems = async () => {
   try {
-    const response = await axios.get("/api/shopping-cart");
+    const response = await axios.get(`${baseUrl}/api/shopping-cart`);
     return response.data;
   } catch (error) {
     console.error("Error fetching shopping cart items:", error);
